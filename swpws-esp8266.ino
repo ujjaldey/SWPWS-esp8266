190fbe929f017f6afdcc79d490c5d54974a69622
Ujjal Dey <ujjaldey@gmail.com>
2021-08-29 19:04:46 +0800

    SWPWS-6 Create api endpoint: /ping

--- swpws-esp8266.ino
+++ swpws-esp8266.ino
@@ -1,13 +1,19 @@
 /*
    swpws-esp8266.ino
 */
-#include "Arduino.h"
+#include <Arduino.h>
 #include <ESP8266WebServer.h>
+#include <ArduinoJWT.h>
+#include <sha256.h>
+#include <ArduinoJson.h>
 
 // Set the WIFI SSID and password
 // Replace with your SSID and password
 const char *ssid = "<your wifi ssid>";
 const char *password = "<your wifi password>";
+
+const char *piUser = "pi";
+const char *piPassword = "raspberry";
 
 // Set Static IP address, Gateway, and Subnet
 // Change if necessary
@@ -16,6 +22,10 @@ IPAddress gateway(192, 168, 10, 1);
 IPAddress subnet(255, 255, 255, 0);
 
 ESP8266WebServer server(80);
+
+ArduinoJWT jwt = ArduinoJWT("secret");
+
+StaticJsonBuffer<200> jsonBuffer;
 
 void setupWifi()
 {
@@ -64,15 +74,114 @@ void setupServer()
   Serial.println("");
 }
 
+String jwtToJsonStr(String jwtStr)
+{
+  int jwtStrLen = jwtStr.length() + 1;
+  char jwtCharArr[jwtStrLen];
+  jwtStr.toCharArray(jwtCharArr, jwtStrLen);
+
+  int jsonStrLen = jwt.getJWTPayloadLength(jwtStr);
+  char jsonStr[jsonStrLen];
+  jwt.decodeJWT(jwtCharArr, jsonStr, jsonStrLen);
+  Serial.println("\nDecoded Payload:");
+  Serial.println(jsonStr);
+
+  return jsonStr;
+}
+
+bool isAuthorized()
+{
+  bool isAuthorized = false;
+  String message = "";
+
+  if (server.hasHeader("Authorization"))
+  {
+    String authHeader = {server.header("Authorization")};
+
+    if (authHeader.indexOf("Bearer") >= 0)
+    {
+      String jwt = authHeader;
+      jwt.replace("Bearer ", "");
+      String jsonStr = jwtToJsonStr(jwt);
+
+      int jsonStrLen = jsonStr.length() + 1;
+      char jsonCharArr[jsonStrLen];
+      jsonStr.toCharArray(jsonCharArr, jsonStrLen);
+
+      JsonObject &root = jsonBuffer.parseObject(jsonCharArr);
+
+      if (root.success())
+      {
+        const char *user = root["user"];
+        const char *password = root["password"];
+
+        Serial.println("*****************");
+        Serial.println(user);
+        Serial.println(password);
+
+        if (strcmp(user, piUser) == 0 && strcmp(password, piPassword) == 0)
+        {
+          isAuthorized = true;
+        }
+        else
+        {
+          message = "Invalid credentials";
+          isAuthorized = false;
+        }
+      }
+      else
+      {
+        message = "Token parsing failed";
+        isAuthorized = false;
+      }
+    }
+    else
+    {
+      message = "No Bearer token found in the request";
+      isAuthorized = false;
+    }
+  }
+  else
+  {
+    message = "No Header found in the request";
+  }
+
+  if (isAuthorized)
+  {
+    return true;
+  }
+  else
+  {
+    Serial.println("ERROR: " + message);
+    server.send(401,
+                "application/json",
+                "{\"success\": false, \"message\": \"" + message + "\"}");
+    return false;
+  }
+}
+
 // Define routing
 void restServerRouting()
 {
-  server.on("/", HTTP_GET, []()
-            { server.send(200, F("application/json"), F("{\"success\": true, \"message\": \"Welcome to SWPWS-ESP8266 REST Web Server\"}")); });
-
-  //  server.on(F("/helloWorld"), HTTP_GET, getHelloWord);
+  server.on("/", HTTP_GET, welcome);
+  server.on(F("/ping"), HTTP_GET, ping);
   //  server.on(F("/on"), HTTP_GET, led_on);
   //  server.on(F("/off"), HTTP_GET, led_off);
+}
+
+void welcome()
+{
+  server.send(200,
+              "application/json",
+              "{\"success\": true, \"message\": \"Welcome to SWPWS-ESP8266 REST Web Server\"}");
+}
+
+void ping()
+{
+  if (isAuthorized())
+  {
+    server.send(200, "text/json", "{\"ping\": \"pong\"}");
+  }
 }
 
 void handleNotFound()
